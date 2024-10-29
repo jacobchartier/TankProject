be008546338ef6a346829d1d51155b39c498fa5d
JacobChartier <55593549+JacobChartier@users.noreply.github.com>
2024-10-29 17:13:43 -0400

    Added input (W, A, S and D)

--- Source/TankProject/Private/Tank.cpp
+++ Source/TankProject/Private/Tank.cpp
@@ -11,3 +11,35 @@ ATank::ATank()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
 	FollowCamera->SetupAttachment(SpringArm);
 }
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+
+		if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+		{
+			EnhancedInputComponent->BindAction(InputActionMove, ETriggerEvent::Triggered, this, &ATank::Move);
+			EnhancedInputComponent->BindAction(InputActionTurn, ETriggerEvent::Triggered, this, &ATank::Turn);
+		}
+	}
+}
+
+void ATank::Move(const FInputActionValue& Value)
+{
+	FVector Movement = FVector::ZeroVector;
+	Movement.X = Value.Get<float>() * TravelSpeed;
+	AddActorLocalOffset(Movement);
+}
+
+void ATank::Turn(const FInputActionValue& Value)
+{
+	FRotator Rotation = FRotator::ZeroRotator;
+	Rotation.Yaw = Value.Get<float>() * RotationSpeed * -1.f;
+	AddActorLocalRotation(Rotation);
+}
--- Source/TankProject/Public/Tank.h
+++ Source/TankProject/Public/Tank.h
@@ -8,11 +8,17 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "Engine/LocalPlayer.h"
+
 #include "Tank.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class TANKPROJECT_API ATank : public AMasterPawn
 {
@@ -21,10 +27,30 @@ class TANKPROJECT_API ATank : public AMasterPawn
 public:
 	ATank();
 
-private:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* InputActionMove;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	float TravelSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* InputActionTurn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	float RotationSpeed;
+
 	UPROPERTY(EditAnywhere)
 	class USpringArmComponent* SpringArm;
 
 	UPROPERTY(EditAnywhere)
 	class UCameraComponent* FollowCamera;
+
+	void Move(const FInputActionValue& Value);
+
+	void Turn(const FInputActionValue& Value);
 };
